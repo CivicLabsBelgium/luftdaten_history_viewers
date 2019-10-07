02b94042f6edf2c50a4cb60907554e8d9cda84e6
toon nelissen appsaloon <toon@appsaloon.be>
2019-10-07 10:46:33 +0200

    fix: routing + add changelog

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Hotspot from './Hotspot'
 function AppRouter() {
   return (
     <Router>
-        <Route exact path="/" render={() => <Redirect to='/map' />} />
-        <Route path="/map" component={Map} />
-        <Route path="/hotspot" component={Hotspot} />
+      <Route exact path="/history_map/" render={() => <Redirect to='/history_map/map' />} />
+      <Route path="/history_map/map" component={Map} />
+      <Route path="/history_map/hotspot" component={Hotspot} />
     </Router>
   )
 }
